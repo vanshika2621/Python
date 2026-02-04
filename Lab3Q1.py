8be4764cd0dede15af2043c25ddded1757afd033
vanshika2621 <vanshikagiri2020@gmail.com>
2026-02-04 11:38:17 +0530

    Create Lab3Q1.py

---
+++ Lab3Q1.py
@@ -0,0 +1,9 @@
+'''1) . print Length of the function without using LEN function'''
+
+s = str(input("Enter your string: "))
+count = 0
+for i in s:
+    count += 1
+
+print("Length of the string is : ", count)
+
